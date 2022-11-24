13a3dc901c342e7c46eeba56433c89739d3f6729
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-11-24 21:12:14 +0200

    ...

--- contacts.mjs
+++ contacts.mjs
@@ -1,27 +1,31 @@
-const fs = require('fs').promises;
-const path = require('path');
-import { nanoid } from 'nanoid';
-import { normalize } from 'node:path/win32';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import shortid from 'shortid';
 
-const contactsPath = './db/contacts.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, './db/', 'contacts.json');
 
 async function readData() {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 }
 
-async function listContacts() {
+export async function listContacts() {
   return await readData();
 }
 
-async function getContactById(contactId) {
+export async function getContactById(contactId) {
   const contacts = await readData();
 
   return contacts.find(contact => contact.id === contactId.toString());
 }
 
-async function removeContact(contactId) {
+export async function removeContact(contactId) {
   const contacts = await readData();
+
   const updatedContacts = contacts.filter(contact => contact.id !== contactId.toString());
 
   await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
@@ -29,15 +33,22 @@ async function removeContact(contactId) {
   return updatedContacts;
 }
 
-async function addContact(name, email, phone) {
+export async function addContact(name, email, phone) {
   const contacts = await readData();
+
+  if (contacts.find(contact => contact.name.toLowerCase() === name.oLowerCase) > -1) {
+    return `${name} already exist in base`;
+  }
+
   const newContact = {
-    id: nanoid(5),
+    id: shortid.generate(5),
     name,
     email,
     phone,
   };
   const updatedContacts = [...contacts, newContact];
+
   await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
+
   return updatedContacts;
 }
